5524f45c234b163ebf90c5098655a351faaa6b84
Ty Burningham <tybret7@gmail.com>
2025-11-10 11:53:45 -0700

    ADD: attributes and functions for classes

--- animal.py
+++ animal.py
@@ -6,12 +6,21 @@ class Animal:
     kingdom = "Animalia"
     # TODO create a class-level attribute that is a list of all the Animal objects
 
-    # TODO create the initializer for Animal with name and species as attributs
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
 
+    def speak(self):
+        print ("The animal makes a noise.")
+
+    def __str__(self):
+        return f"Kingdom: {self.kingdom}, Name: {self.name} Species: {self.species}"
+    
     # TODO: Add a method to make a generic sound 
     # Call the method `speak` and make it output a specific message like 
     # "The animal makes a noise.""
 
     # TODO __str__ method for string representation
     # Example output
-    # Kingdom: 'kingdom attribute', Name: 'name attribute' Species: 'species attribute' 
+    # Kingdom: 'kingdom attribute', Name: 'name attribute' Species: 'species attribute'
+    
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from animal import Animal
+from dog import Dog
 
 if __name__ == "__main__":
-    # TODO: Create an instance of the Animal class
+
+    kitty = Animal("Kitty", "Feline")
+    fido = Dog("Fido", "Canine", "Bulldog")
+
+    print (kitty)
+    print (fido)
+    kitty.speak()
+    fido.speak()
     # TODO: Print the Animal instance
     # TODO: Call the method to make a generic animal sound
 
@@ -9,3 +18,4 @@ if __name__ == "__main__":
     # TODO: Call the method to make the dog-specific sound
 
     # TODO print out all the animals
+    
