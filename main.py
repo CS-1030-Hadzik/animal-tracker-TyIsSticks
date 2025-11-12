d2444c2167cb782d0dcbfe050c255ebb3ef4e7f4
Ty Burningham <tybret7@gmail.com>
2025-11-12 11:33:55 -0700

    Finish project

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ if __name__ == "__main__":
     fido = Dog("Fido", "Canine", "Bulldog")
 
     print (kitty)
-    print (fido)
     kitty.speak()
+
+    print (fido)
     fido.speak()
-    # TODO: Print the Animal instance
-    # TODO: Call the method to make a generic animal sound
 
-    # TODO: Create an instance of the Dog class
-    # TODO: Print the Dog instance
-    # TODO: Call the method to make the dog-specific sound
 
-    # TODO print out all the animals
+    print("\nAll tracked animals:")
+    for animal in Animal.all_animals:
+        print(f"  - {animal}")
+    
     
